63fc3ee2ba4a0047069a78b7d7e4d23a7b185c4c
precurzor <jpharrison@pm.me>
2025-01-22 18:21:21 +0000

    Working word count added

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 def main():
     book_path = "books/frankenstein.txt"
-    
     with open(book_path) as f:
         file_contents = f.read()
-        print(file_contents)
+    word_count = count_words(file_contents)
+
+    print(f"Reading {book_path}")
+    print(f"Word Count = {word_count}")
+
+def count_words(file_contents):
+    count = 0
+    words = file_contents.split()
+    for word in words:
+        count += 1    
+    return count
 
 main()
